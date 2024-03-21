210034f96cc9c05498e23234b17a18dafdb827d2
dkpark10 <dkpark10@naver.com>
2024-03-21 21:58:29 +0900

    chore: 디버거 세팅

---
+++ src/components/dynamic-component.jsx
@@ -0,0 +1,3 @@
+export default function DynamicComponent() {
+  return <div>123</div>;
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
 import LazyHydrate from "react-lazy-hydration";
 // import LazyHydrate from "@/components/react-lz-hydrate";
 import dynamic from "next/dynamic";
+import Image from 'next/image';
 
 const LazySwiper = dynamic(() => import("@/components/lazy-component"));
+
+const DynamicComponent = dynamic(() => import("@/components/dynamic-component"));
 
 export const getServerSideProps = async () => {
   const data = await new Promise((resolve) => {
@@ -25,6 +28,8 @@ export default function PageLazy({ data }) {
           {item + 1}
         </div>
       ))}
+      <DynamicComponent />
+      <Image src="/mangom.jpg" width={100} height={100} alt="망곰 이미지" />
       <LazyHydrate whenVisible>
         <LazySwiper />
       </LazyHydrate>
